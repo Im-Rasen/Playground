5e563e1cc7611e13caadf097d68a5dbaa6185488
Rasen <arch_yu@mail.ru>
2019-11-28 19:01:25 +0300

    Initial Commit

---
+++ Playground/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Playground
+//
+//  Created by Julia Alekhina on 28/11/2019.
+//  Copyright © 2019 room_1341. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
